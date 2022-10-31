08de6e2aaad0d3433623ead67d5cbb6c6181d299
Stefan Tosic <tosic.ra29.2019@uns.ac.rs>
2022-10-31 18:18:22 +0100

    create spring boot starter project

--- BloodBank/src/main/java/com/isa/BloodBank/BloodBankApplication.java
+++ BloodBank/src/main/java/com/isa/BloodBank/BloodBankApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.isa.BloodBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BloodBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BloodBankApplication.class, args);
 	}
 
 }
--- BloodBank/src/test/java/com/isa/BloodBank/BloodBankApplicationTests.java
+++ BloodBank/src/test/java/com/isa/BloodBank/BloodBankApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.isa.BloodBank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BloodBankApplicationTests {
 
 	@Test
 	void contextLoads() {
