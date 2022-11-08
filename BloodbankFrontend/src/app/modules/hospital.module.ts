8fb0f2edcd9a0c931d873221b5c561e948d7967f
Stefan Tosic <tosic.ra29.2019@uns.ac.rs>
2022-11-08 17:26:37 +0100

    remove duplicate import

---
+++ BloodbankFrontend/src/app/modules/hospital.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from 'src/app/material/material.module';
+import { BloodBankCenterComponent } from './blood-bank-center/blood-bank-center.component';
+import { SearchUsersComponent } from './search-users/search-users.component';
+import { UserSearchFilterPipe } from './pipes/user-search-filter.pipe';
+
+const routes: Routes = [
+    { path: 'blood-bank-center/:id', component: BloodBankCenterComponent },
+    { path: 'search-users', component: SearchUsersComponent}
+  ];
+  @NgModule({
+    declarations: [
+      SearchUsersComponent,
+      UserSearchFilterPipe
+    ],
+    imports: [
+      CommonModule,
+      MaterialModule,
+      FormsModule,
+      ReactiveFormsModule,
+      RouterModule.forChild(routes)
+    ],
+    exports: [RouterModule],
+  })
+  export class HospitalModule {}
