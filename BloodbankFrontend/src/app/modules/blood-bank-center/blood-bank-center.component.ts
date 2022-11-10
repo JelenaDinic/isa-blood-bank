0b04c80c3b88e77f0f42c137920b78e42cf66b8b
Jelena Dinić <jelenaad26@gmail.com>
2022-11-10 14:56:44 +0100

    added list of staff in bloodbank

--- BloodbankFrontend/src/app/modules/blood-bank-center/blood-bank-center.component.ts
+++ BloodbankFrontend/src/app/modules/blood-bank-center/blood-bank-center.component.ts
@@ -1,7 +1,9 @@
+import { UserDisplayDTO } from './../dto/userDisplayDTO';
 import { BloodBankCenter } from './../model/blood-bank-center.model';
 import { BloodBankService } from './../services/blood-bank-center.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StaffService } from '../services/staff.service';
 
 @Component({
   selector: 'app-blood-bank-center',
@@ -11,13 +13,19 @@ import { Router } from '@angular/router';
 export class BloodBankCenterComponent implements OnInit {
 
   public bloodBankCenter: BloodBankCenter = new BloodBankCenter;
+  public staffList: any;
 
-  constructor(private bloodBankCenterService: BloodBankService, private router: Router) { }
+  constructor(private bloodBankCenterService: BloodBankService, private staffService: StaffService, private router: Router) { }
 
   ngOnInit(): void {
-        this.bloodBankCenterService.getBloodBankCenter(1).subscribe(res => {
+        this.bloodBankCenterService.getBloodBankCenter(4).subscribe(res => {
           this.bloodBankCenter = res;
+          this.staffService.getStaffByCenterId(this.bloodBankCenter.id).subscribe(res => {
+            this.staffList = res;
+            console.log(this.staffList)
+          })
         })
+        
   }
   public update(bloodBankCenter: BloodBankCenter): void {
     if (!this.isValidInput())
