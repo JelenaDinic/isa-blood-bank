a85e790b7d4e3d1d75cdc69b39df3065a2152380
vanjaftn <vanjateodorovic00@gmail.com>
2022-11-18 02:21:43 +0100

    add validation for user registration

--- BloodBank/src/main/java/com/isa/BloodBank/dto/BloodDonorCreationDTO.java
+++ BloodBank/src/main/java/com/isa/BloodBank/dto/BloodDonorCreationDTO.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,17 +15,52 @@ import lombok.Setter;
 
 public class BloodDonorCreationDTO {
 
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isUnacceptableWeight;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isUnhealthy;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String hasSkinProblems;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String hasUnacceptablePressure;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isUnderTreatment;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isOnPeriod;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String hasTattoo;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isRested;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isEpileptic;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String hasAllergies;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String hasDrankAlcohol;
+
+    @NotNull
+    @NotBlank(message = "You must choose one answer")
     private String isPregnant;
 
 }
--- BloodBank/src/main/java/com/isa/BloodBank/dto/UserCreationDTO.java
+++ BloodBank/src/main/java/com/isa/BloodBank/dto/UserCreationDTO.java
@@ -4,6 +4,7 @@ import com.isa.BloodBank.model.UserRole;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -11,19 +12,64 @@ import java.time.LocalDate;
 
 public class UserCreationDTO {
 
+        @NotNull
+        @NotBlank(message = "First name cannot be blank")
+        @Pattern(regexp="([A-Z][a-z]+)(\\s[A-Z]?[a-z]+)*", message="Invalid firstname input!")
         private String firstName;
+
+        @NotNull
+        @NotBlank(message = "Last name cannot be blank")
+        @Pattern(regexp="([A-Z][a-z]+)(\\s[A-Z]?[a-z]+)*", message="Invalid lastname input!")
         private String lastName;
+        @NotNull
+        @NotBlank(message = "Email cannot be blank")
+        @Pattern(regexp = "(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$", message = "Invalid email format")
         private String email;
+
+        @NotNull
+        @NotBlank(message = "Password cannot be blank")
         public String password;
+
+        @NotNull
         private UserRole role;
+
+        @NotNull
+        @NotBlank(message = "Street cannot be blank")
         private String street;
+
+        @NotNull
+        @NotBlank(message = "Street number cannot be blank")
         private String number;
+
+        @NotNull
+        @NotBlank(message = "City cannot be blank")
         private String city;
+
+        @NotNull
+        @NotBlank(message = "Country cannot be blank")
         private String country;
+
+        @NotNull
+        @Past(message = "Date of birth must be in the past")
         private LocalDate dob;
+
+        @NotNull
+        @NotBlank(message = "Phone number cannot be blank")
         private String phoneNumber;
+
+        @NotNull
+        @NotBlank(message = "Gender cannot be blank")
         private String gender;
+
+        @NotNull
+        @NotBlank(message = "Profession cannot be blank")
         private String profession;
+
+        @NotNull
+        @NotBlank(message = "Profession information cannot be blank")
         private String professionInfo;
+
+        @NotNull
+        @Digits(integer = 13, fraction = 0, message = "Personal id must be exactly 13 digits")
         private Long personalId;
     }
