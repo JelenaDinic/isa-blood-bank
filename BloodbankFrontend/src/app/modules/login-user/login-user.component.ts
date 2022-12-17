e21dbcc1c787070557d3a89295e2e732eaa83a55
vanjaftn <vanjateodorovic00@gmail.com>
2022-12-17 17:39:33 +0100

    add changes on frontend

--- BloodbankFrontend/src/app/modules/login-user/login-user.component.ts
+++ BloodbankFrontend/src/app/modules/login-user/login-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { LoginUser } from '../model/login-user.model';
 import { LoginService } from '../services/login.service';
 
@@ -11,16 +12,25 @@ export class LoginUserComponent implements OnInit {
 
   public loginUser : LoginUser = new LoginUser();
   public token : string = "";
-  constructor(private loginService: LoginService) { }
+  public role : string = "";
+  constructor(private toastr: ToastrService, private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
   Login(){
-    this.loginService.login(this.loginUser).subscribe((response: string) => {
+    this.loginService.login(this.loginUser).subscribe((response: any) => {
       this.token = response;
       localStorage.setItem('token',this.token);
       console.log(this.token)
+      let decodedJWT = JSON.parse(window.atob(this.token.split('.')[1]));
+      this.role = decodedJWT.role.authority;
+      localStorage.setItem('loggedUserRole', this.role);
+      console.log(this.role);
+      this.toastr.success('Successfully logged in');
+      if(this.role == 'ROLE_STAFF')
+      window.location.href = '/staff-profile';
+
     },
     (error) => {
       console.log(error.message);
