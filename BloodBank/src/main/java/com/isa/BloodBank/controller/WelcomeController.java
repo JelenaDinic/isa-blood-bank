497383aae5a8e455a1bbb07a8501a9cbf3df391c
vanjaftn <vanjateodorovic00@gmail.com>
2022-12-19 02:26:38 +0100

    add email sender

--- BloodBank/src/main/java/com/isa/BloodBank/controller/WelcomeController.java
+++ BloodBank/src/main/java/com/isa/BloodBank/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package com.isa.BloodBank.controller;
 
 import com.isa.BloodBank.model.AuthRequest;
 import com.isa.BloodBank.Util.JwtUtil;
+import com.isa.BloodBank.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,6 +16,9 @@ public class WelcomeController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private EmailSenderService emailSenderService;
+
     @GetMapping("/")
     public String welcome() {
         return "Welcome to javatechie !!";
@@ -24,6 +28,7 @@ public class WelcomeController {
     @PostMapping("/authenticate")
     public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
+            emailSenderService.sendSimpleEmail("vanjateodorovic00@gmail.com", "legendo", "ti si legenda");
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
             );
